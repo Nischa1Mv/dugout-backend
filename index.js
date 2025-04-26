e0a6c23831fbb569e13d410190d7fb058df400ad
NischalMantri <sainischalmv@gmail.com>
2025-04-26 14:35:31 +0530

    basic server setup of dugout

---
+++ index.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import app from "./app.js"
+
+const startServer=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+startServer()
