a0047a52f25ef45a9e23f1666db3db949f04b03f
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-07-20 15:38:58 +0530

    replaced mongoose setup with prisma - postgress -supavbase

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import login from "./login.js";
-import signup from "./signup.js"
 
 const app = express();
 app.use(express.json());
@@ -11,6 +9,4 @@ app.get('/', function (_, res) {
     res.send('working');
 });
 
-app.use('/login',login);
-app.use('/signup', signup);
 export default app;
